df4edc989da66f476593251aa05b5a6bd81f533b
vsergunswork-creator <v.serguns.work@gmail.com>
2026-01-10 20:30:29 +0100

    Delete test.js

--- functions/api/test.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest() {
-  return new Response(
-    JSON.stringify({ status: "ok", message: "Functions work" }),
-    {
-      headers: { "Content-Type": "application/json" }
-    }
-  );
-}
